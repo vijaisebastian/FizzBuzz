37639858efcbf4d17b21d40bd7cf501e47be251b
vijaisebastian <vijaisebastian@gmail.com>
2024-03-27 16:49:16 +0000

    C# code written from the test case for divisible by 3 scenario(Fizz)

---
+++ FizzBuzz.App/FizzCheck.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FizzBuzz.App
+{
+    public class FizzCheck
+    {
+
+        public bool Validate(int number)
+        {
+            return number % 3 == 0;
+        }
+
+        public string GetString()
+        {
+            return "Fizz";
+        }
+    }
+}
--- FizzBuzz.Test/FizzBuzzTest.cs
+++ FizzBuzz.Test/FizzBuzzTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using FizzBuzz.App;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
 namespace FizzBuzz.Test
